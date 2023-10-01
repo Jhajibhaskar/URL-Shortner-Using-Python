a03b06829997017c7d8da5764ae4a7ba8d6b5185
Ujjwal Kumar <ujjwalkumar949494@gmail.com>
2023-10-01 16:08:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 #First run 'pip install pyshorteners' in terminal of pycharm then execute the below program
+import streamlit as st
+import validators
 import pyshorteners
 
-url = input('Enter Any Webpage address to Short: ')
+def validate_url(url):
+    # Add http:// prefix if missing
+    if not url.startswith(('http://', 'https://')):
+        url = 'http://' + url
 
+    return validators.url(url)
 
-def shortenurl(url):
-    s = pyshorteners.Shortener()
-    print('Your Shorted URL is: ',s.tinyurl.short(url))
+def shorten_url(long_url):
+    shortener = pyshorteners.Shortener()
+    short_url = shortener.tinyurl.short(long_url)
+    return short_url
 
-shortenurl(url)
+st.title("URL Shortener")
+
+long_url = st.text_input("Enter Long URL")
+
+if st.button("Shorten URL"):
+    if long_url:
+        if validate_url(long_url):
+            short_url = shorten_url(long_url)
+            st.success(f"Shortened URL: {short_url}")
+        else:
+            st.error("Invalid URL. Please enter a valid URL.")
+
